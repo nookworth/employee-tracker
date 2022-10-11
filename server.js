00ce7888f388e0d76b7f66b41de8c21b5742d992
nookworth <nooksack@gmail.com>
2022-10-11 03:30:29 -0600

    First three bonus questions complete.

--- server.js
+++ server.js
@@ -36,10 +36,16 @@ const init = () => {
             "View all departments",
             "View all roles",
             "View all employees",
+            "View employees by manager",
+            "View employees by department",
             "Add a department",
             "Add a role",
             "Add an employee",
+            "Delete a department",
+            "Delete a role",
+            "Delete an employee",
             "Update an employee role",
+            "Update an employee's manager",
           ],
         },
 
@@ -63,24 +69,12 @@ const init = () => {
           message: "Please enter the salary for the role",
           name: "roleSalary",
           when: (answers) => answers.roleTitle,
-          // validate: (answer) => {
-          //   if (typeof answer != Number) {
-          //     return "Please enter your answer as a number with no commas";
-          //   }
-          //   return true;
-          // },
         },
         {
           type: "input",
           message: "Please enter the department ID in which the role exists",
           name: "roleDept",
           when: (answers) => answers.roleSalary,
-          // validate: (answer) => {
-          //   if (typeof answer != Number) {
-          //     return "Please enter your answer as an integer";
-          //   }
-          //   return true;
-          // },
         },
 
         // Group of questions about adding an employee
@@ -108,43 +102,160 @@ const init = () => {
           name: "employeeManager",
           when: (answers) => answers.employeeRole,
         },
+
+        //Question for deleting a department
+        {
+          type: "input",
+          message: "Enter the ID of the department you wish to delete.",
+          name: "deleteDept",
+          when: (answers) => answers.menu == "Delete a department",
+        },
+
+        //Group of questions for updating an employee
+        {
+          type: "input",
+          message: "Please enter the ID of the employee you wish to update.",
+          name: "employeeUpdate",
+          when: (answers) => answers.menu == "Update an employee role",
+        },
+        {
+          type: "input",
+          message: "Please enter the ID of the new role for this employee.",
+          name: "newRole",
+          when: (answers) => answers.employeeUpdate,
+        },
       ])
+
       //One .then handles all possible answers
       .then((response) => {
         if (response.menu == "View all departments") {
           viewDepartments();
-        }
-        if (response.menu == "View all roles") {
+        } else if (response.menu == "View all roles") {
           viewRoles();
-        }
-        if (response.menu == "View all employees") {
+        } else if (response.menu == "View all employees") {
           viewEmployees();
-        }
-        if (response.department) {
+        } else if (response.menu == "View employees by manager") {
+          viewEmployeesByManager();
+        } else if (response.menu == "View employees by department") {
+          viewEmployeesByDept();
+        } else if (response.department) {
           addDepartment(JSON.stringify(response.department));
         }
+
         // Call the addRole function when the last question in the role chain is finished
-        if (response.roleDept) {
+        else if (response.roleDept) {
           addRole(
             JSON.stringify(response.roleTitle),
             response.roleSalary,
             response.roleDept
           );
         }
+
         //Call the addEmployee function when the last question in the employee chain is finished
-        if (response.employeeManager) {
+        else if (response.employeeManager) {
           addEmployee(
             JSON.stringify(response.employeeName),
             JSON.stringify(response.employeeSurname),
             response.employeeRole,
             response.employeeManager
           );
+
+          //Call the deleteDept function if the option is chosen
+        } else if (response.deleteDept) {
+          deleteDept(response.deleteDept);
+        }
+
+        //Call the updateEmployee function when the last question in the update chain is finished
+        else if (response.newRole) {
+          newRole();
+        } else if (response.menu == "Update an employee's manager") {
+          newManager();
         }
       })
   );
 };
 
 init();
+
+const newRole = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message:
+          "Please enter the ID of the employee whose role you wish to update.",
+        name: "employee",
+      },
+      {
+        type: "input",
+        message: "Please enter the ID of the new role for this employee.",
+        name: "role",
+      },
+    ])
+    .then((response) => {
+      updateEmployeeRole(response.employee, response.role);
+    });
+};
+
+const newManager = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message:
+          "Please enter the ID of the employee whose manager you wish to update.",
+        name: "employee",
+      },
+      {
+        type: "input",
+        message: "Please enter the ID of the new manager for this employee.",
+        name: "manager",
+      },
+    ])
+    .then((response) => {
+      updateEmployeeManager(response.employee, response.manager);
+    });
+};
+
+// const update = () => {
+//   db.query(
+//     "SELECT COUNT(*) AS idCount FROM employee",
+//     function (err, rows, results) {
+//       idCount = rows[0].idCount;
+//       console.log(idCount);
+//     }
+//   );
+
+//   db.query(
+//     `SELECT * FROM employee WHERE id = ${idCount}`,
+//     function (err, results) {
+//       console.log(results);
+//     }
+//   );
+
+// for (let i = 1; i < idCount + 1; i++) {
+//   db.query(`SELECT * FROM employee WHERE id = ${i}`, function (err, results) {
+//     console.log(results);
+//   });
+// }
+
+// return inquirer.prompt([
+//   //Group of questions for updating an employee
+//   {
+//     type: "list",
+//     message: "Please select the ID of the employee you wish to update.",
+//     name: "employeeUpdate",
+//     choices: [],
+//     when: (answers) => (answers.menu = "Update an employee role"),
+//   },
+//   {
+//     type: "input",
+//     message: "Please enter the ID of the new role for this employee.",
+//     name: "newRole",
+//     when: (answers) => answers.employeeUpdate,
+//   },
+// ]);
+// };
 
 const viewDepartments = () => {
   db.query("SELECT * FROM department", function (err, results) {
@@ -162,6 +273,24 @@ const viewEmployees = () => {
   db.query("SELECT * FROM employee", function (err, results) {
     console.table(results);
   });
+};
+
+const viewEmployeesByManager = () => {
+  db.query(
+    "SELECT manager_id, id, first_name, last_name, role_id FROM employee ORDER BY manager_id",
+    function (err, results) {
+      console.table(results);
+    }
+  );
+};
+
+const viewEmployeesByDept = () => {
+  db.query(
+    "SELECT department.id AS department_id, employee.id AS employee_id, employee.first_name, employee.last_name FROM employee INNER JOIN role ON role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY department.id;",
+    function (err, results) {
+      console.table(results);
+    }
+  );
 };
 
 const addDepartment = (departmentName) => {
@@ -183,6 +312,31 @@ const addRole = (title, salary, department_id) => {
 const addEmployee = (first_name, last_name, role_id, manager_id) => {
   db.query(
     `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (${first_name}, ${last_name}, ${role_id}, ${manager_id})`
+  );
+  db.query("SELECT * FROM employee", function (err, results) {
+    console.table(results);
+  });
+};
+
+const deleteDept = (id) => {
+  db.query(`DELETE FROM department WHERE id = ${id}`);
+  db.query("SELECT * FROM department", function (err, results) {
+    console.table(results);
+  });
+};
+
+const updateEmployeeRole = (employee_id, role_id) => {
+  db.query(
+    `UPDATE employee SET role_id = ${role_id} WHERE id = ${employee_id}`
+  );
+  db.query("SELECT * FROM employee", function (err, results) {
+    console.table(results);
+  });
+};
+
+const updateEmployeeManager = (employee_id, manager_id) => {
+  db.query(
+    `UPDATE employee SET manager_id = ${manager_id} WHERE id = ${employee_id}`
   );
   db.query("SELECT * FROM employee", function (err, results) {
     console.table(results);
